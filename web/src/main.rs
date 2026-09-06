8e0bf3a527cc979b8f237aafdc670323c236e939
Paul Nettleton <k9@k9withabone.dev>
2026-09-06 16:44:30 -0500

    feat(web): use tailwindcss for styling
    In the `format` CI job, the `leptosfmt --experimental-tailwind` option
    is used to ensure consistent ordering of Tailwind utility classes.
    
    Signed-off-by: Paul Nettleton <k9@k9withabone.dev>

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,7 +1,13 @@
 //! `novelnote_web` is the web frontend for NovelNote, a self-hosted book tracker.
 
-use leptos::{html::ElementChild, mount::mount_to_body, view};
+use leptos::{attr::global::ClassAttribute, html::ElementChild, mount::mount_to_body, view};
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> });
+    mount_to_body(|| {
+        view! {
+            <main class="flex justify-center items-center h-screen text-black dark:text-white bg-slate-50 dark:bg-slate-800">
+                <h1 class="text-6xl font-bold sm:text-8xl">"NovelNote"</h1>
+            </main>
+        }
+    });
 }
